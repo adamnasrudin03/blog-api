ad5b573e76b6e8759ac7aba95ecd360a32bbce6d
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-28 10:55:26 +0700

    delete  field name

--- entity/blog.go
+++ entity/blog.go
@@ -5,7 +5,6 @@ import "time"
 //Blog represents blogs table in database
 type Blog struct {
 	ID       		uint64  	`gorm:"primary_key:auto_increment" json:"id"`
-	Name     		string 		`gorm:"type:varchar(255)" json:"name"`
 	Author     		string 		`gorm:"type:varchar(255)" json:"author"`
 	Title     		string 		`gorm:"type:varchar(255)" json:"title"`
 	Description     string 		`gorm:"type:varchar(255)" json:"description"`
