13bdb9e1f38396b9cda87d47ecfac24a2f158f0e
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-28 13:15:12 +0700

    repo method findall, findbyid, deletebyid, update

--- repository/blog.go
+++ repository/blog.go
@@ -8,7 +8,11 @@ import (
 
 //BlogRepository is a declaration contract
 type BlogRepository interface {
-	Save(book entity.Blog) (entity.Blog, error)
+	Save(blog entity.Blog) (entity.Blog, error)
+	FindAll() ([]entity.Blog, error)
+	FindByID(blogID uint64) (entity.Blog, error)
+	Update(blog entity.Blog) (entity.Blog, error)
+	DeleteByID(blogID uint64) (entity.Blog, error)
 }
 
 
@@ -24,6 +28,52 @@ func NewBlogRepository(db *gorm.DB) *blogRepository {
 //implement method BlogRepository, func to save directly to db
 func (r *blogRepository) Save(blog entity.Blog) (entity.Blog, error) {
 	err := r.db.Create(&blog).Error
+	if err != nil {
+		return blog, err
+	}
+
+	return blog, nil
+}
+
+//implement method BlogRepository, func to get all blog directly to db
+func (r *blogRepository) FindAll() ([]entity.Blog, error) {
+	var blogs []entity.Blog
+
+	err := r.db.Find(&blogs).Error
+	if err != nil {
+		return blogs, err
+	}
+
+	return blogs, nil
+}
+
+//implement method BlogRepository, func to get by id blog directly to db
+func (r *blogRepository) FindByID(blogID uint64) (entity.Blog, error){
+	var blog entity.Blog
+
+	err := r.db.Where("id = ?", blogID).Find(&blog).Error
+	if err != nil {
+		return blog, err
+	}
+
+	return blog, nil
+}
+
+//implement method BlogRepository, func to update blog directly to db
+func (r *blogRepository) Update(blog entity.Blog) (entity.Blog, error) {
+	err := r.db.Save(&blog).Error
+	if err != nil {
+		return blog, err
+	}
+
+	return blog, nil
+}
+
+//implement method BlogRepository, func to delete by id blog directly to db
+func (r *blogRepository) DeleteByID(blogID uint64) (entity.Blog, error) {
+	var blog entity.Blog
+	
+	err := r.db.Where("id = ?", blogID).Delete(&blog).Error
 	if err != nil {
 		return blog, err
 	}
