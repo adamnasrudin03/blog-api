a100a01623a9bb7a4d52fd8fffdb4f045151cb64
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-28 18:35:25 +0700

    comment repo method findById and update

--- repository/comment.go
+++ repository/comment.go
@@ -9,8 +9,9 @@ import (
 //CommentRepository is a declaration contract
 type CommentRepository interface {
 	Save(comment entity.Comment) (entity.Comment, error)
+	FindByID(commentID uint64) (entity.Comment, error)
+	Update(comment entity.Comment) (entity.Comment, error) 
 }
-
 
 type commentRepository struct {
 	db *gorm.DB
@@ -24,6 +25,28 @@ func NewCommentRepository(db *gorm.DB) *commentRepository {
 //implement method CommentRepository, func to save directly to db
 func (r *commentRepository) Save(comment entity.Comment) (entity.Comment, error) {
 	err := r.db.Create(&comment).Error
+	if err != nil {
+		return comment, err
+	}
+
+	return comment, nil
+}
+
+//implement method CommentRepository, func to get by id comment directly to db
+func (r *commentRepository) FindByID(commentID uint64) (entity.Comment, error){
+	var comment entity.Comment
+
+	err := r.db.Where("id = ?", commentID).Find(&comment).Error
+	if err != nil {
+		return comment, err
+	}
+
+	return comment, nil
+}
+
+//implement method CommentRepository, func to update comment directly to db
+func (r *commentRepository) Update(comment entity.Comment) (entity.Comment, error) {
+	err := r.db.Save(&comment).Error
 	if err != nil {
 		return comment, err
 	}
