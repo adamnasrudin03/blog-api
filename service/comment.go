34b596bb2e5da625888bb2584a6e876a3ac0d8cc
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-28 18:39:17 +0700

    service comment, method UpdateComment

--- service/comment.go
+++ service/comment.go
@@ -9,6 +9,7 @@ import (
 //CommentService is a declaration contract
 type CommentService interface {
 	CreateComment(input dto.CreateComment) (entity.Comment, error)
+	UpdateComment(commentID uint64, input dto.CreateComment) (entity.Comment, error)
 }
 
 type commentService struct {
@@ -36,4 +37,25 @@ func (s *commentService) CreateComment(input dto.CreateComment) (entity.Comment,
 
 	return newComment, nil
 
+}
+
+//implement method CommentService, func to take input from the user then update it / continue to the repository (update)
+func (s *commentService) UpdateComment(commentID uint64, input dto.CreateComment) (entity.Comment, error){
+	//checks with the FindById method in the package repository, which returns data and error values
+	comment, err := s.commentRepository.FindByID(commentID)
+	if err != nil {
+		return comment, err
+	}
+
+	comment.Author = input.Author
+	comment.Comments = input.Comments
+	comment.BlogID = input.BlogID
+
+	//proceed to the Update method in the package repository, which returns the data and error values
+	newComment, err := s.commentRepository.Update(comment)
+	if err != nil {
+		return newComment, err
+	}
+
+	return newComment, nil
 }
