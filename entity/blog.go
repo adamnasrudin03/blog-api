c1dc1e7b2202ec7b7eb8283924d294c2b89f13a5
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-28 14:47:57 +0700

    edit entity blog

--- entity/blog.go
+++ entity/blog.go
@@ -5,9 +5,12 @@ import "time"
 //Blog represents blogs table in database
 type Blog struct {
 	ID       		uint64  	`gorm:"primary_key:auto_increment" json:"id"`
+	CommentID     	uint64  	`json:"-"`
 	Author     		string 		`gorm:"type:varchar(255)" json:"author"`
 	Title     		string 		`gorm:"type:varchar(255)" json:"title"`
 	Description     string 		`gorm:"type:text" json:"description"`
 	CreatedAt   	time.Time 	`json:"-"`
 	UpdatedAt   	time.Time	`json:"-"`
+	Comment			[]Comment	`gorm:"constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"comments"`
+
 }
