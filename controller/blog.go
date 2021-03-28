b1dbe1f4c057b6037a072a126564c9b209f051c3
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-28 11:49:43 +0700

    implement method create Blog in controller/handler

--- controller/blog.go
+++ controller/blog.go
@@ -1,15 +1,49 @@
 package controller
 
-import "blog-api/service"
+import (
+	"blog-api/dto"
+	"blog-api/helper"
+	"blog-api/service"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+//BlogController is a declaration contract
 type BlogController interface {
-
+	CreateBlog(ctx *gin.Context)
 }
 
 type blogController struct {
 	blogService service.BlogService
 }
 
+//NewBlogController create a new instances of BlogController
 func NewBlogController(blogService service.BlogService) *blogController {
 	return &blogController{blogService}
+}
+
+
+func (c *blogController) CreateBlog(ctx *gin.Context) {
+	var input dto.CreateBlog
+
+	err := ctx.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to create blog", http.StatusUnprocessableEntity, "error", errorMessage)
+		ctx.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newBlog, err := c.blogService.CreateBlog(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create blog", http.StatusBadRequest, "error", nil)
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create blog", http.StatusOK, "success", newBlog)
+	ctx.JSON(http.StatusOK, response)
 }
